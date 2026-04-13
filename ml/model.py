0bd98d4dafa13439cd464e76369039e82e22b689
LGib1 <lgibbo1@wgu.edu>
2026-04-13 14:44:44 -0600

    Code added to model.py

--- ml/model.py
+++ ml/model.py
@@ -14,7 +14,9 @@ from typing import Union
 
 
 # Optional: implement hyperparameter tuning.
-def train_model(X_train, y_train):
+def train_model(X_train: Union[pd.DataFrame, np.ndarray], 
+                y_train: Union[pd.Series, np.ndarray],
+                parameters: dict = None) -> LogisticRegression:
     """
     Trains a machine learning model and returns it.
 
@@ -30,10 +32,16 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    clf = LogisticRegression(random_state=72925)
+    scorer = make_scorer(fbeta_score,  beta=1)
+    grid_object = GridSearchCV(clf, parameters, scoring=scorer, n_jobs=-1, cv=5)
+    grid_fit = grid_object.fit(X_train, y_train)
+    best_clf = grid_fit.best_estimator_
+    best_clf.fit(X_train, y_train)
 
+    return best_clf
 
-def compute_model_metrics(y, preds):
+def compute_model_metrics(y: Union[pd.Series, np.ndarray], preds: Union[pd.Series, np.ndarray]):
     """
     Validates the trained machine learning model using precision, recall, and F1.
 
@@ -55,12 +63,12 @@ def compute_model_metrics(y, preds):
     return precision, recall, fbeta
 
 
-def inference(model, X):
+def inference(model: LogisticRegression, X: Union[pd.DataFrame, np.ndarray]) -> Union[np.ndarray, pd.Series]:
     """ Run model inferences and return the predictions.
 
     Inputs
     ------
-    model : ???
+    model : RandomForestClassifier
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -70,9 +78,9 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
-    pass
+    return model.predict(X)
 
-def save_model(model, path):
+def save_model(model, path: str) -> None:
     """ Serializes model to a file.
 
     Inputs
@@ -83,13 +91,19 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
-    pass
+    #pass
+    if os.path.exists(path):
+        os.unlink(path) #remove the file if it already exists
+    with open(path, 'wb') as pickle_file:
+        pickle.dump(model, pickle_file)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
-    pass
-
+    #pass
+    with open(path, 'rb') as pickle_file:
+        model = pickle.load(pickle_file)
+    return model
 
 def performance_on_categorical_slice(
     data, column_name, slice_value, categorical_features, label, encoder, lb, model
@@ -117,7 +131,7 @@ def performance_on_categorical_slice(
         Trained sklearn OneHotEncoder, only used if training=False.
     lb : sklearn.preprocessing._label.LabelBinarizer
         Trained sklearn LabelBinarizer, only used if training=False.
-    model : ???
+    model : RandomFrostClassifier
         Model used for the task.
 
     Returns
@@ -132,7 +146,14 @@ def performance_on_categorical_slice(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
+        sliced_data, 
+        categorical_features, 
+        label, 
+        encoder, 
+        lb, 
+        training=False, 
+        std_scaler=std_scaler
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model=model, X=X_slice) #None # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
